4204936a7a1e5a3f9e3b74274d44a9a45d86fb84
siyual-park <siyual.bak@gmail.com>
2020-11-10 11:59:20 +0900

    build

---
+++ lib/cjs/index.d.ts
@@ -0,0 +1,6 @@
+export { JsonConvert } from "./src/json2typescript/json-convert";
+export { JsonCustomConvert } from "./src/json2typescript/json-custom-convert";
+export { ValueCheckingMode, OperationMode } from "./src/json2typescript/json-convert-enums";
+export { JsonObject, JsonProperty, JsonConverter } from "./src/json2typescript/json-convert-decorators";
+export { Any } from "./src/json2typescript/any";
+export { Settings, MappingOptions } from "./src/json2typescript/json-convert-options";
