00de183128a0362281eebb8ac18a31d3ce516958
Ara Park <siyual.bak@gmail.com>
2020-11-10 11:59:37 +0900

    feature: export_mapping_options (#3)
    * feature/export_mapping_options
    
    * build

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -3,3 +3,4 @@ export { JsonCustomConvert } from "./src/json2typescript/json-custom-convert";
 export { ValueCheckingMode, OperationMode } from "./src/json2typescript/json-convert-enums";
 export { JsonObject, JsonProperty, JsonConverter } from "./src/json2typescript/json-convert-decorators";
 export { Any } from "./src/json2typescript/any";
+export { Settings, MappingOptions } from "./src/json2typescript/json-convert-options";
